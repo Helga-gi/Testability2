8ed92e891d12ddd82b9ac3e5d0d9a7ac96fc9b73
Helga test <www.helga@mail.ru>
2023-04-19 13:05:17 +0300

    NewClass

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double height = 1.87;
+        System.out.println("Укажите рост в метрах: ");
+        int weight = 98;
+        System.out.println("Укажите вес в килограммах: ");
+        int bmi = service.calculate( height, weight );
+
+        System.out.println("Индекс: " + bmi);
+    }
+}
